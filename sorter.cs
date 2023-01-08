440a0033e3c658c8cce05d932e5d1c749b349a6f
alexeybliznyuk <66327586+alexeybliznyuk@users.noreply.github.com>
2023-01-09 01:31:32 +0300

    idk how it works
    Idk how it works, must be debugged, answer is wrong but next to right

---
+++ sorter.cs
@@ -0,0 +1,64 @@
+using System.Globalization;
+
+int[] nums = { 54, 7, 41, 23, 4, 12, 80, 33, 5, 2 };
+int temp = nums[0];
+int[] new_list = new int[nums.Length];
+int temp_num = 0;
+int[] func_res = new int[3];
+
+
+
+int[] Find_less(int[] nums, int temp,int temp_num)
+{
+    for (int i = 0; i < nums.Length; i++)
+    {
+        if (nums[i] != 0)
+        {
+            if (temp >= nums[i])
+            {
+                temp = nums[i];
+                temp_num = i;
+                Console.WriteLine(temp);
+            }
+        }
+    }
+    Console.WriteLine($"temp_num: {temp_num}");
+    int[] result = { temp, temp_num };
+    return result;
+    //temp is liest number
+}
+
+
+
+//Find_less(nums, temp, temp_num);
+//nums[4] = 12;
+/*
+int[] func_res = Find_less(nums, temp, temp_num);
+Console.WriteLine("func_ress:");
+Console.WriteLine(func_res[0]);
+Console.WriteLine(func_res[1]);
+new_list[0] = func_res[0];
+nums[func_res[1]] = 0;
+func_res = Find_less(nums, temp, temp_num);
+Console.WriteLine("func_ress:");
+Console.WriteLine(func_res[0]);
+Console.WriteLine(func_res[1]);
+*/
+
+
+for (int j = 0; j < new_list.Length; j++)
+{
+    func_res = Find_less(nums, temp, temp_num);
+    new_list[j] = func_res[0];
+    nums[func_res[1]] = 0;
+}
+
+
+for (int i = 0; i < nums.Length - 1; i++)
+{
+    Console.Write(nums[i]);
+}
+for (int i = 0; i < nums.Length - 1; i++)
+{
+    Console.Write(new_list[i]);
+}
